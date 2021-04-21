6d141db8cfb16b35b1b8cae6f5f05be7b227f5c4
digikar99 <shubhamkarayare@gmail.com>
2021-04-22 03:22:55 +0530

    [fix] avoid alexandria:type=

--- package.lisp
+++ package.lisp
@@ -3,6 +3,7 @@
   (:import-from
    :introspect-environment
    :constant-form-value)
+  (:shadow :type=)
   (:import-from
    :fiveam
    :def-test
@@ -10,3 +11,9 @@
   (:export
    :form-type
    :form-typep))
+
+(in-package :trivial-form-type)
+
+(defun type= (type1 type2 &optional env)
+  (and (subtypep type1 type2 env)
+       (subtypep type2 type1 env)))
